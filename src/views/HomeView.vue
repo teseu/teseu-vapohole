b40c187aedf4675f45b369f6a8afb64522b06206
Tulio Macedo <tuliomac@gmail.com>
2023-09-20 20:53:46 -0300

    🎨 Limpeza do cód inicial vue
    Incluindo minha própria estrutura

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,9 @@
 <script setup>
-import TheWelcome from '../components/TheWelcome.vue'
+
 </script>
 
 <template>
-  <main>
-    <TheWelcome />
+  <main class="w-[80vw] mx-auto">
+    <img alt="Teseu logo" class="logo" src="@/assets/teseu.png" width="125" height="125" />
   </main>
 </template>
