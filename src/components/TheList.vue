58a4438f867f0cbaeb066b206cc195627fb67f67
Tulio Macedo <tuliomac@gmail.com>
2023-09-21 05:35:50 -0300

    ✨ Pág about
    preenchida em parte e planejada uma 2ª parte

--- src/components/TheList.vue
+++ src/components/TheList.vue
@@ -4,8 +4,8 @@ const props = defineProps(['item']);
 </script>
 
 <template>
-  <li class="flex items-center">
-    <PhCheckFat class="mr-4" color="#059669" :size="32" weight="fill" />
+  <li class="flex items-center mb-2">
+    <PhCheckFat class="mr-4" color="#059669" :size="32" weight="duotone" />
     <span class="text-lg">{{ item }}</span>
   </li>
 </template>
--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,9 +1,44 @@
+<script setup>
+import { ref } from 'vue'
+import Card from '@/components/ui/TheCard.vue'
+import List from '@/components/TheList.vue'
+
+const items = ref([
+  {
+    id: 1,
+    item: 'Vuejs 3'
+  },
+  {
+    id: 2,
+    item: 'Vite'
+  },
+  {
+    id: 3,
+    item: 'TailwindCSS'
+  },
+  {
+    id: 4,
+    item: 'Neovide com AstroVim'
+  }
+])
+</script>
+
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
+  <div class="flex flex-col">
+    <div class="flex flex-col gap-4">
+      <h1 class="text-3xl mb-4">Sobre esta página</h1>
+      <Card>
+        <template #titulo>Tecnologias</template>
+        <ul class="text-xl">
+          <List v-for="item in items" :key="item.id" :item="item.item">
+          </List>
+        </ul>
+      </Card>
+    </div>
+    <div class="flex gap-4 mt-6">
+      <h2 class="text-2xl mb-4">Alguns screenshots</h2>
+    </div>
   </div>
 </template>
 
-<style>
-
-</style>
+<style></style>
