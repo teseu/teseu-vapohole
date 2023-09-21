eeeacf9a4d20df3cc83703c832735e4b268567a7
Tulio Macedo <tuliomac@gmail.com>
2023-09-21 05:38:18 -0300

    🎨 Preparando 1º build
    build para subir 1ª versão

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -35,8 +35,9 @@ const items = ref([
         </ul>
       </Card>
     </div>
-    <div class="flex gap-4 mt-6">
+    <div class="flex flex-col gap-4 mt-6">
       <h2 class="text-2xl mb-4">Alguns screenshots</h2>
+      <div class="text-xl mt-4">Em breve</div>
     </div>
   </div>
 </template>
